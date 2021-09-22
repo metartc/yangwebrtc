39aa0d666b12cf8bd6dfaed07233306df9c16f69
yangrtc <yangrtc@aliyun.com>
2021-09-22 20:26:34 +0800

    2.0

--- YangPlayer2.0/src/video/YangVideoWinHandleH.h
+++
@@ -1,7 +0,0 @@
-#ifndef YANGVIDEOWINHANDLEH_H
-#define YANGVIDEOWINHANDLEH_H
-#include <QWidget>
-void yang_clearRender();
-void yang_add_winvideo(int pwid,QWidget * pwin);
-void yang_add_winshowvideo(int pwid,int puid);
-#endif // YANGVIDEOWINHANDLEH_H
