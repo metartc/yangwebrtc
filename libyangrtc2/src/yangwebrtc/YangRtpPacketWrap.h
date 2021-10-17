4d8b93371924c32c012f23632d828b900aa0ae23
yangrtc <yangrtc@aliyun.com>
2021-10-17 18:20:21 +0800

    2.0.031

---
+++ libyangrtc2/src/yangwebrtc/YangRtpPacketWrap.h
@@ -0,0 +1,39 @@
+﻿#ifndef SRC_YANGRTP_YANGRTPPACKETWRAP_H_
+#define SRC_YANGRTP_YANGRTPPACKETWRAP_H_
+#include <yangrtp/YangRtp.h>
+#include <yangrtp/YangRtpBuffer.h>
+#include <yangrtp/YangRtpRawPayload.h>
+#include <yangrtp/YangRtpSTAPPayload.h>
+#include <yangrtp/YangRtpFUAPayload2.h>
+#include <yangstream/YangStreamCapture.h>
+#include <yangwebrtc/YangRtcSessionI.h>
+
+class YangRtpPacketWrap {
+public:
+	YangRtpPacketWrap();
+	virtual ~YangRtpPacketWrap();
+	void init(YangRtpBuffer* rtpBuffer,YangRtcSessionI* psession);
+	int32_t on_h264_video(YangStreamCapture* videoFrame);
+	int32_t on_audio(YangStreamCapture* audioFrame);
+	int32_t package_single_nalu(char* p,int32_t plen, int64_t timestamp);
+	int32_t package_single_nalu(YangStreamCapture* videoFrame);
+	int32_t package_fu_a(YangStreamCapture* videoFrame,int32_t fu_payload_size);
+	int32_t package_stap_a(YangStreamCapture* videoFrame);
+public:
+	uint32_t m_videoSsrc;
+	uint32_t m_audioSsrc;
+	uint16_t m_videoSeq;
+	uint16_t m_audioSeq;
+private:
+	YangRtpBuffer* m_rtpBuffer;
+	YangRtcSessionI* m_session;
+	YangRtpFUAPayload2 m_fua2;
+	YangRtpRawPayload m_raw;
+	YangRtpSTAPPayload m_stap;
+
+	char* m_videoBuf;
+	YangRtpPacket m_pushVideoPacket;
+	YangRtpPacket m_pushAudioPacket;
+	int32_t encode(YangRtpPacket *pkt);
+};
+#endif /* SRC_YANGRTP_YANGRTPPACKETWRAP_H_ */
