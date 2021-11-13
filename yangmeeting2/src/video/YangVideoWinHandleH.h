ad6eb1b67421c3b718279754cc624851513d9a1d
yangrtc <87118023+yangrtc@users.noreply.github.com>
2021-11-13 12:40:06 +0800

    Delete yangmeeting2 directory

--- yangmeeting2/src/video/YangVideoWinHandleH.h
+++
@@ -1,7 +0,0 @@
-#ifndef YANGVIDEOWINHANDLEH_H
-#define YANGVIDEOWINHANDLEH_H
-#include "../yangplayer/YangPlayWidget.h"
-void yang_clearRender();
-void yang_add_winvideo(int pwid,YangPlayWidget * pwin);
-void yang_add_winshowvideo(int pwid,int puid);
-#endif // YANGVIDEOWINHANDLEH_H
